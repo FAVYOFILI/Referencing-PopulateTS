2704b066c1a12df5faf7dd17f3c0b0e1d6d115a4
Lucky C. Ofili <lcofili2000@gmail.com>
2025-09-15 10:30:06 +0100

    MongoDB Atlas

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -10,13 +10,9 @@ import {
   deleteAllAdmins,
   GetAll
 } from "../controller/adminController";
-// import { authMiddleware } from "../middleware/userMiddleware";
-// import { userLoggerMiddleware } from "../middleware/userLoggerMiddleware";
 
 export const AdminRouter: Router = express.Router();
 
-// AdminRouter.use(userLoggerMiddleware);
-// AdminRouter.use(authMiddleware)
 
 AdminRouter.post("/signupadmin", SignUpAdmin);
 AdminRouter.post("/loginadmin", LoginAdmin);
